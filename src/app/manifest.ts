d66d8e93bab288a09d01d9ec9a363ac3c6a3b9f3
Ratul Hasan Ruhan <ratulhasan1644@gmail.com>
2026-04-09 00:17:30 +0600

    Update TypeScript configuration and refine manifest asset purposes
    - Added baseUrl to tsconfig.json for improved module resolution.
    - Updated purpose attribute in manifest.ts for better clarity on image usage.

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -18,13 +18,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/web-app-manifest-192x192.png",
         type: "image/png",
         sizes: "192x192",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
       {
         src: "/web-app-manifest-512x512.png",
         type: "image/png",
         sizes: "512x512",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
     ],
   };
